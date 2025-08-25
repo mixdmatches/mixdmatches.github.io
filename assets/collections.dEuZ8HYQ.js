393153ac98d4ad6e3f8cd57fda44eef410a90e4d
mixdmatches <mixdmatches@users.noreply.github.com>
2025-08-25 16:40:26 +0000

    deploy: 6810db24b0824cca4def0612b6373db814402ab1

---
+++ assets/collections.dEuZ8HYQ.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.BKD-an8q.js";import{u as l}from"./chunks/vue-router.JwRDX0SR.js";import{M as t}from"./framework.CFM6qow-.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
